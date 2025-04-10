fb593ee03db9a76cd3a85d5cf07b0e2afc6f423c
RemiLecas <remi.lecas@ecole-isitech.fr>
2025-04-10 11:06:21 +0200

    Refacto dossier

---
+++ TP/EventManagerAPI-TP/Migrations/20250408144902_modif_bdd.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagerAPI_TP.Migrations
+{
+    /// <inheritdoc />
+    public partial class modif_bdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Events",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_RoomId",
+                table: "Events",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Rooms_RoomId",
+                table: "Events",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Categories_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Rooms_RoomId",
+                table: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_RoomId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Events");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Events",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "Other")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ TP/EventManagerAPI-TP/Migrations/20250409091616_modif_bdd_et_ajout_seeder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagerAPI_TP.Migrations
+{
+    /// <inheritdoc />
+    public partial class modif_bdd_et_ajout_seeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Locations",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Locations");
+        }
+    }
+}
